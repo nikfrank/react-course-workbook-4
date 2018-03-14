e1cddfcc5c5d5eb6ec78a2c57d7e754122ccf5c4
nik frank <nikbfrank@gmail.com>
2018-03-14 14:56:43 +0200

    testing for network calls basic

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import fakeFetch from './fakeFetch';
+
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+test('that submit button makes call to fetch', (done)=>{
+  const calledApi = jest.fn();
+  
+  fakeFetch.loadMock('getXrate', {
+    status: 200,
+    body: { EUR: 700 },
+    pattern: /^https:\/\/min-api.cryptocompare.com\/data\/price\?fsym=/,
+    callback: calledApi,
+  });
+
+  const p = mount(<App/>);
+
+  p.find('button').first().simulate('click');
+
+  setTimeout(()=> {
+    expect(calledApi.mock.calls).toHaveLength( 1 );
+    done();
+  }, 0);
 });
---
+++ src/fakeFetch.js
@@ -0,0 +1,27 @@
+const defaultResult = { status: 404, message: 'not found', pattern: /^\/$/ };
+
+const fetchify = result => ({
+  ...result,
+  json: ()=> Promise.resolve( result.body ),
+});
+
+export default new (class FakeFetch {
+  mocks = {}
+  oldFetch = window.fetch
+  
+  fetch = window.fetch = (...args) =>
+    Promise.resolve(
+      Object.keys(this.mocks)
+            .find( mock=> this.mocks[mock].pattern.exec(args[0]) ) ||
+      this.oldFetch(...args)
+    ).then( result => this.mocks[result] ? fetchify(this.mocks[result]) : result
+    ).then( result => (result.callback && result.callback(), result ) )
+  
+  loadMock = (name, result=defaultResult)=>
+    this.mocks[name] || (this.mocks[name] = result)
+
+  unloadMock = name => {
+    delete this.mocks[name];
+    if( !Object.keys(this.mocks).length ) window.fetch = this.oldFetch;
+  }
+});
