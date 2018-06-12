2c53fb20cd7f10946553c77121b08b7e643e6b39
nik frank <nikbfrank@gmail.com>
2018-06-12 12:26:45 +0300

    more example

--- src/CurrencyChart.js
+++ src/CurrencyChart.js
@@ -4,7 +4,7 @@ import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'r
 const exampleLineChartData = [
   {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
   {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
-  {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
+  {name: 'Page C', pv: 9800, amt: 2290},
   {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
   {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
   {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
@@ -15,9 +15,8 @@ const SimpleLineChart = ({ trades })=> {
 
   // here convert trades
 
-console.log(JSON.stringify(trades));
-
-  const exampleTrade = [
+  console.log(JSON.stringify(trades));
+  const exampleTrades = [
     {
       "fromCoin":"ETH",
       "toCoin":"WINGS",
@@ -25,7 +24,16 @@ console.log(JSON.stringify(trades));
       "toAmount":17110.3,
       "date":1528791856000,
     },
+    {
+      "fromCoin":"WINGS",
+      "toCoin":"ETH",
+      "toAmount":10,
+      "fromAmount":17110.3,
+      "date":1528791957000,
+    },
   ];
+
+  
   
   return (
     <LineChart width={600} height={300} data={exampleLineChartData}
@@ -36,7 +44,7 @@ console.log(JSON.stringify(trades));
       <Tooltip/>
       <Legend />
       <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{r: 8}}/>
-      <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+      <Line connectNulls={true} type="monotone" dataKey="uv" stroke="#82ca9d" />
     </LineChart>
   );
 };
